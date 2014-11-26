f8d08187536f76b662fa6499703287f697340321
kevin14 <kevin14@me.com>
2014-11-26 11:05:43 +0800

    add market,user package

--- campus/settings.py
+++ campus/settings.py
@@ -36,7 +36,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'campusServer',
+    'market',
+    'user',
 )
 
 MIDDLEWARE_CLASSES = (
