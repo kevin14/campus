33b4cadc0a696e445a5e64281652441cc4f71901
zhuoyangzong <zhuoyangzong@zhuoyangzongdeMacBook-Pro.local>
2014-12-09 09:46:56 +0800

    添加集市模型

--- market/models.py
+++ market/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
 # Create your models here.
+
+
+class CpcGoods(models.Model):
+    uid = models.IntegerField(max_length=10)
+    gname = models.CharField(max_length=16)
+    gdesc = models.TextField()
+    gprice = models.IntegerField(max_length=10)
+    mobile = models.IntegerField(max_length=15)
+    status = models.SmallIntegerField(max_length=1)
+    place = models.CharField(max_length=20, default=0)
+    browserNum = models.IntegerField(max_length=10)
+    classify = models.SmallIntegerField(max_length=2)
+    gpicUrl = models.CharField(max_length=100)
+    createAt = models.IntegerField(max_length=10)
+    updateAt = models.IntegerField(max_length=10)
+
+
+class CpcPics(models.Model):
+    picUrl = models.CharField(max_length=30)
+    createAt = models.IntegerField(max_length=10)
+    updateAt = models.IntegerField(max_length=10)
+
+
+class CpcComments(models.Model):
+    uid = models.IntegerField(max_length=10)
+    gid = models.IntegerField(max_length=10)
+    comments = models.CharField(max_length=255)
+    pid = models.IntegerField(max_length=10)
+    createAt = models.IntegerField(max_length=10)
+    updateAt = models.IntegerField(max_length=10)
