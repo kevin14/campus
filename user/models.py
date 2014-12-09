d3c214246083016dc257a4e6c0da900fb6bd68a6
zhuoyangzong <zhuoyangzong@zhuoyangzongdeMacBook-Pro.local>
2014-12-09 09:46:16 +0800

    添加用户模型

--- user/models.py
+++ user/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+
+class CpcUser(models.Model):
+    userName = models.CharField(max_length=16)
+    email = models.CharField(max_length=100)
+    password = models.CharField(max_length=32)
+    ipAddress = models.CharField(max_length=15)
+    createAt = models.IntegerField(max_length=10)
+    updateAt = models.IntegerField(max_length=10)
+
+    class Meta:
+        db_table = 'cpc_user'
+
+
+class CpcUserInfo(models.Model):
+    uAvatar = models.CharField(max_length=255)
+    schoolId = models.SmallIntegerField(max_length=5)
+    gender = models.SmallIntegerField(max_length=1, null=True, blank=True)
+    createAt = models.IntegerField(max_length=10, null=True, blank=True)
+    updateAt = models.IntegerField(max_length=10)
+
+    class Meta:
+        db_table = 'cpc_userinfo'
